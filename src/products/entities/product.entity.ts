e5d7134c45c859f5d8b252a50aaf1ba816bbed4f
Fabio Arias <fabio-adrian-arias@hotmail.com.ar>
2023-05-09 11:47:02 -0300

    CreateDTOProduct
    Agrege DTO- Class-Validator-Class-Transformer- services.products y products. actualice diferentes modulos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
 
 import {Column,Entity,PrimaryGeneratedColumn} from "typeorm";
+
 @Entity()
 export class Product{
     @PrimaryGeneratedColumn("uuid")
@@ -7,5 +8,34 @@ export class Product{
     @Column("text",{
         unique:true,
     })
-    title:string
+    title:string;
+
+    @Column("numeric",{
+        default:0
+    })
+    price: number;
+    
+    @Column({
+        type: "text",
+        nullable: true
+    })
+    description: string;
+
+    @Column("text",{
+        unique:true,
+    })
+    slug: string;
+
+    @Column('int',{
+      default:0,
+    })  
+    stock:number;
+
+    @Column("text",{
+        array:true,
+    })
+    sizes:string[];
+    
+    @Column('text')
+    gender:string;
 }
