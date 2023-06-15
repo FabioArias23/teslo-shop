8f2a23fea9cc1509481e835bdf8b8a58a387ca3e
Fabio Arias <fabio-adrian-arias@hotmail.com.ar>
2023-06-15 10:21:23 -0300

    JwtStrategy&PassportModule, JwtModule

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,43 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "../interfaces/jwt-payload.interfaces";
+import { User } from "../entities/users.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
+import { Repository } from "typeorm";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy( Strategy){
+
+    constructor(
+        @InjectRepository( User )
+        private readonly userRepository: Repository<User>,
+
+        configService: ConfigService
+    ){
+        super({
+            secretOrKey: configService.get('JWT_SECRET'),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        })
+    }
+
+
+    async validate(payload:JwtPayload): Promise<User>{
+        const { email } = payload
+        
+        const user = await this.userRepository.findOneBy({email})
+        
+        
+        if (!user)
+        throw new UnauthorizedException('Token not valid')
+
+        if(!user.isActive)
+        
+        throw new UnauthorizedException('user is inactive, talk with an admin')
+        
+        Request
+        return user;
+    }
+    
+}
