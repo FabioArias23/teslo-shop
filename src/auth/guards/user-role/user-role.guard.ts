65c94e545dae7a0297f997863837a681c575b210
Fabio Arias <83869719+FabioArias23@users.noreply.github.com>
2023-07-11 08:57:07 -0300

    VerificarRoll

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { User } from 'src/auth/entities/users.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -15,7 +16,20 @@ export class UserRoleGuard implements CanActivate {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler())
 
-    console.log({validRoles});
-    return true;
+    if(!validRoles) return true;
+    if(validRoles.length === 0 ) return true
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+    if(!user)
+          throw new BadRequestException('User not found');
+      for (const role of user.roles) {
+        if (validRoles.includes(role)) {
+                  return true;
+           }
+      }
+   throw new ForbiddenException( 
+    `User${user.fullName} need a valid role :[${validRoles}]`
+   )
   }
-}
+} 
